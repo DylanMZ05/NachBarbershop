8ad66e5060de65cd56c99034207d1304579a5cb1
Dylan Mazur Zgainer <136861567+DylanMZ05@users.noreply.github.com>
2024-01-31 06:12:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const headerLinks = document.querySelectorAll(".header-button a");
-
-  headerLinks.forEach(function(link) {
-      link.addEventListener("click", function(event) {
-          event.preventDefault();
-
-          // Obtén el objetivo del enlace (por ejemplo, "#servicios")
-          const targetId = link.getAttribute("href").substring(1);
-          const targetElement = document.getElementById(targetId);
-
-          if (targetElement) {
-              const delay = 0; // Retraso de 3 segundos en milisegundos
-              const offsetPixels = 70;
-
-              const finalPosition = targetElement.offsetTop - offsetPixels;
-
-              // Espera el tiempo especificado antes de desplazarte
-              setTimeout(function() {
-                  // Desplázate al elemento de destino
-                  window.scrollTo({
-                      top: finalPosition,
-                      behavior: "smooth" // Agrega desplazamiento suave
-                  });
-              }, delay);
-          }
-      });
-  });
+document.getElementById('miBoton').addEventListener('click', function() {
+    // Lógica que se ejecutará al hacer clic en el botón
+    alert('¡Botón clicado!');
 });
